75c9a719a9c469dbd114b2c4065743110183410b
Matthew Fishman <mfishman@flatironinstitute.org>
2022-03-28 09:46:19 -0400

    Add compat for ITensors 0.3. Fix example, add PyCall PySCF example.

--- examples/02_integrals.jl
+++ examples/02_integrals.jl
@@ -1,8 +1,22 @@
 using ITensors
 using ITensorPySCF
 
-# Create Simple Molecule
-mol = gto.M(atom="N 0 0 0; N 0 0 1;", basis="sto3g", verbose=3)
+molecule_N2 = "
+N 0 0 0;
+N 0 0 1;
+"
+
+molecule_H20 = "
+O 1.2091536548 1.7664118189 -0.0171613972;
+H 2.1984800075 1.7977100627 0.0121161719;
+H 0.9197881882 2.458018557 0.629793883;
+"
+
+molecule = molecule_H20
+
+basis = "sto3g"
+
+mol = gto.M(atom=molecule, basis=basis, verbose=3)
 
 # Run HF
 mf = scf.RHF(mol).run()
@@ -28,7 +42,12 @@ e_nuclear = mf.energy_nuc()
 
 n = size(t, 1)
 V = reshape(V, n, n, n, n)
+
+# convert from chemistry convention to physics convention
+V = 0.5 * permutedims(V, (3, 2, 1, 4))
+
 os = OpSum()
+os += e_nuclear, "Id", 1
 for i in 1:n, j in 1:n
   os .+= t[i, j], "Cdagup", i, "Cup", j
   os .+= t[i, j], "Cdagdn", i, "Cdn", j
@@ -43,15 +62,18 @@ end
 s = siteinds("Electron", n; conserve_nf=true)
 H = MPO(os, s)
 
-occupation_to_state(n) = n == 0 ? 1 : (n == 2 ? 4 : error("Occupation is $n"))
+occ_to_state = Dict([0 => 1, 2 => 4])
+occupation_to_state(n) = occ_to_state[n]
 ψmf = MPS(s, occupation_to_state.(n_occ))
-@show inner(ψmf, H, ψmf)
+@show inner(ψmf', H, ψmf)
 
-ψ0 = randomMPS(s, occupation_to_state.(n_occ); linkdims=40)
-@show inner(ψ0, H, ψ0)
+ψ0 = randomMPS(s, occupation_to_state.(n_occ); linkdims=1)
+@show inner(ψ0', H, ψ0)
 
 sweeps = Sweeps(10)
 setmaxdim!(sweeps, 100, 200)
 setcutoff!(sweeps, 1e-6)
 setnoise!(sweeps, 1e-6, 1e-7, 1e-8, 0.0)
 e, ψ = dmrg(H, ψ0, sweeps)
+
+@show e - cisolver.e_tot
